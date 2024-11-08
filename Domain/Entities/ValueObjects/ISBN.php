0c813455c50d763c69833a89c11b6999ec0f35de
Miriam Maranhão Da Silva <98363007+im-maranhao@users.noreply.github.com>
2024-11-08 01:07:19 -0300

    validate isbn

---
+++ Domain/Entities/ValueObjects/ISBN.php
@@ -0,0 +1,27 @@
+<?PHP
+// biblioteca/Domain/ValueObjects/ISBN.php
+
+namespace ValueObjects;
+
+class ISBN
+{
+    private string $isbn;
+
+    public function __construct(string $isbn)
+    {
+        if(!$this->isValidISBN($isbn)){
+            throw new \InvalidArgumentException("Invalid ISBN.");
+        }
+        $this->isbn = $isbn;
+    }
+
+    private function isValidISBN(string $isbn): bool
+    {
+        return true;
+    }
+
+    public function __toString(): string
+    {
+        return $this->isbn;
+    }
+}
